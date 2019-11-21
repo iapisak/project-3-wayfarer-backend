8ece50a06601f8f9d2e89aa2de01c6e00480e0f6
potisanmetee <iapisak1981@gmail.com>
2019-11-20 21:12:26 -0800

    Add dotenv, connect-mongo, express-session and cors

--- server.js
+++ server.js
@@ -4,11 +4,33 @@ const PORT = 4000
 const db = require('./models')
 const routes = require('./routes/api')
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
+require('dotenv').config();
+
 app.listen(PORT, () => {console.log(`We on ${PORT}`)})
 
 
+const corsOptions = {
+    origin:['http://localhost:3000'],
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+  
+app.use(cors(corsOptions))
+
 // ------MiddleWare---//
 app.use(bodyParser.json());
+app.use(session({
+    store: new MongoStore({ url: process.env.MONGO_URI }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60,
+    }
+  }));
 
 // -------Routes-----//
 

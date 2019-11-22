7a99a55aad81eed747e522caaf7ca30d8478e542
potisanmetee <iapisak1981@gmail.com>
2019-11-22 10:42:07 -0800

    tes city routes

--- routes/api.js
+++ routes/api.js
@@ -33,7 +33,7 @@ router.get('/users/:id', ctrl.user.getUserInfo);
 
 
 // Post Routes : Create, Update, Delete, Get
-router.post('/posts',ctrl.post.createPost)
+router.post('/cities/:city_slug/posts/new',ctrl.post.createPost);
 
 router.get('/posts', ctrl.post.allPosts)
 
@@ -43,7 +43,7 @@ router.delete('/posts/:postId/delete', ctrl.post.deletePost);
 
 // Cities //
 
-router.put('/cities/new', ctrl.city.createcity)
+router.post('/cities/new', ctrl.city.createcity)
 
 router.get('/cities', ctrl.city.allCities)
 router.get('/cities/:city_id/posts', ctrl.city.allPostsOfCity)

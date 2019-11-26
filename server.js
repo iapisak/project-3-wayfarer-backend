0deb1770dec1dbf5f61aec7d6d1b78ae1a6eaf2a
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-11-26 09:59:48 -0800

    Fix hardcoded port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 const db = require('./models')
 const routes = require('./routes/api')
 const bodyParser = require('body-parser');
